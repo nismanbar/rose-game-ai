1676d40d5c8ab663e4a09bb348459f25f58d5be9
yzamir <kobi.zamir@gmail.com>
2024-01-10 19:32:50 +0200

    lint fix
    Signed-off-by: yzamir <kobi.zamir@gmail.com>

--- examples/driver.py
+++ examples/driver.py
@@ -7,26 +7,26 @@ driver_name = "Driver"
 
 
 def drive(world):
-   """
-   This method controls the car's movement based on the obstacle it encounters.
+    """
+    This method controls the car's movement based on the obstacle it encounters.
 
-   Parameters:
-   world (World): The game world object containing the car and obstacles.
+    Parameters:
+    world (World): The game world object containing the car and obstacles.
 
-   Returns:
-   Action: An action to perform based on the obstacle encountered.
-   """
-   x = world.car.x
-   y = world.car.y
-   obstacle = world.get((x, y - 1))
+    Returns:
+    Action: An action to perform based on the obstacle encountered.
+    """
+    x = world.car.x
+    y = world.car.y
+    obstacle = world.get((x, y - 1))
 
-   if obstacle == obstacles.PENGUIN:
-       return actions.PICKUP
-   elif obstacle == obstacles.WATER:
-       return actions.BRAKE
-   elif obstacle == obstacles.CRACK:
-       return actions.JUMP
-   elif obstacle == obstacles.NONE:
-       return actions.NONE
-   else:
-       return actions.RIGHT if (x % 3) == 0 else actions.LEFT
+    if obstacle == obstacles.PENGUIN:
+        return actions.PICKUP
+    elif obstacle == obstacles.WATER:
+        return actions.BRAKE
+    elif obstacle == obstacles.CRACK:
+        return actions.JUMP
+    elif obstacle == obstacles.NONE:
+        return actions.NONE
+    else:
+        return actions.RIGHT if (x % 3) == 0 else actions.LEFT
