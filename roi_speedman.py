c73f70e7f3d4ba34ffbac45bd4cb9899c298fbce
Eran Shoham <eranshoham08@gmail.com>
2025-07-21 14:48:12 +0300

    Added something that might work.

--- roi_speedman.py
+++ roi_speedman.py
@@ -4,7 +4,7 @@ This driver implements some logic.
 
 from rose.common import obstacles, actions  # NOQA
 
-driver_name = "MCQUEEN-2"
+driver_name = "MCQUEEN-Different3"
 
 
 def drive(world):
@@ -21,55 +21,68 @@ def drive(world):
     y = world.car.y
     obstacle = world.get((x, y - 1))
 
-    obstacles_matrix = [
-        [world.get((0, y - 1)), world.get((1, y - 1)), world.get((2, y - 1))],
-        [world.get((0, y - 2)), world.get((1, y - 2)), world.get((2, y - 2))],
-        [world.get((0, y - 3)), world.get((1, y - 3)), world.get((2, y - 3))],
-        [world.get((0, y - 4)), world.get((1, y - 4)), world.get((2, y - 4))],
-        [world.get((0, y - 5)), world.get((1, y - 5)), world.get((2, y - 5))],
-        [world.get((0, y - 6)), world.get((1, y - 6)), world.get((2, y - 6))]]
+    # obstacles_matrix = []
+    # for i in range(3 * x // 3, 3 * x // 3 + 3):
+    #     obstacles_matrix.append([])
+    #     for j in range(8):
+    #         obstacles_matrix[i].append(world.get((i, j)))
+    #
+    # forward = most_point_path(obstacles_matrix, 0, x, y)
+    # if x % 3 == 0:
+    #     left = -100
+    # else:
+    #     left = most_point_path(obstacles_matrix, 0, x - 1, y)
+    # if x % 3 == 2:
+    #     right = -100
+    # else:
+    #     right = most_point_path(obstacles_matrix, 0, x + 1, y)
+    # if right > forward and right > left:
+    #     return actions.RIGHT
+    # elif left > forward and left > right:
+    #     return actions.LEFT
 
-    if obstacle == obstacles.PENGUIN:
+    chosen_action = find_path(world, x, y)
+    if chosen_action != actions.NONE:
+        print(chosen_action)
+        return chosen_action
+    elif obstacle == obstacles.PENGUIN:
+        print("Penguin!")
         return actions.PICKUP
     elif obstacle == obstacles.WATER:
+        print("Stop!")
         return actions.BRAKE
     elif obstacle == obstacles.CRACK:
+        print("Jump!")
         return actions.JUMP
     elif obstacle == obstacles.NONE:
+        print("Nothing!")
         return actions.NONE
     else:
+        print("Turn!")
         return actions.RIGHT if (x % 3) == 0 else actions.LEFT
 
 
-def most_point_path(obstacles_matrix, points, x, y) -> list:
+def most_point_path(obstacles_matrix, points, x, y):
     if y == 0:
         return points
-    if x == 1:
-        middle_obstacles = [obstacles_matrix[y-1][x-1], obstacles_matrix[y-1][x], obstacles_matrix[y-1][x+1]]
-        left_points = []
-        forward_points = []
-        right_points = []
-        possible_points = [left_points, forward_points, right_points]
-        options = [actions.LEFT, actions.NONE, actions.RIGHT]
-
-
-
-    forward = most_point_path(obstacles_matrix, points, x, y - 1)
-    if x <= 1:
-        right = most_point_path(obstacles_matrix, points, x - 1, y - 1)
+    forward_points = calculate_forward_points(obstacles_matrix[x][y - 1])
+    forward = most_point_path(obstacles_matrix, points + forward_points, x, y - 1)
+    if x % 3 != 0:
+        left_points = calculate_side_points(obstacles_matrix[x - 1][y - 1])
+        right = most_point_path(obstacles_matrix, points + left_points, x - 1, y - 1)
     else:
-        right = 0
-    if x >= 1:
-        left = most_point_path(obstacles_matrix, points, x + 1, y - 1)
+        right = [0]
+    if x % 3 != 2:
+        right_points = calculate_side_points(obstacles_matrix[x + 1][y - 1])
+        left = most_point_path(obstacles_matrix, points + right_points, x + 1, y - 1)
     else:
-        left = 0
+        left = [0]
 
-
-    if forward[0] > right[0] and forward[0] > left[0]:
-        return forward
+    if forward > right and forward > left:
+        return forward, actions.NONE
     elif right > left:
-        return right
-    return left
+        return right, actions.RIGHT
+    return left, actions.LEFT
 
 
 def find_next_obstacle(world, x, car_y):
@@ -83,7 +96,16 @@ def find_next_obstacle(world, x, car_y):
     return obstacle, y
 
 
-def calculate_points(obstacle):
+def calculate_side_points(obstacle):
+    if obstacle == obstacles.NONE:
+        return 0
+    elif obstacle == obstacles.PENGUIN:
+        return 10
+    else:
+        return -10
+
+
+def calculate_forward_points(obstacle):
     if obstacle == obstacles.NONE:
         return 0
     elif obstacle == obstacles.CRACK:
@@ -94,3 +116,65 @@ def calculate_points(obstacle):
         return 10
     else:
         return -10
+
+
+def find_path(world, x, y):
+    print("Shalom!")
+    # Forward:
+    current_y = y
+    obstacle = world.get((x, current_y - 1))
+    current_points = calculate_forward_points(obstacle)
+    forward_points = current_points
+    while current_points != -10 and current_y > 0:
+        forward_points += current_points
+        current_y -= 1
+        obstacle = world.get((x, current_y - 1))
+        current_points = calculate_forward_points(obstacle)
+    forward_y = current_y
+
+    # Left:
+    if x % 3 != 0:
+        current_y = y
+        obstacle = world.get((x - 1, current_y - 1))
+        current_points = calculate_side_points(obstacle)
+        left_points = current_points
+        while current_points != -10 and current_y > 0:
+            left_points += current_points
+            current_y -= 1
+            obstacle = world.get((x - 1, current_y - 1))
+            current_points = calculate_forward_points(obstacle)
+        left_y = current_y
+    else:
+        left_points = -100
+        left_y = y
+
+    # Right:
+    if x % 3 != 2:
+        current_y = y
+        obstacle = world.get((x + 1, current_y - 1))
+        current_points = calculate_side_points(obstacle)
+        right_points = current_points
+        while current_points != -10 and current_y > 0:
+            right_points += current_points
+            current_y -= 1
+            obstacle = world.get((x + 1, current_y - 1))
+            current_points = calculate_forward_points(obstacle)
+        right_y = current_y
+    else:
+        right_points = -100
+        right_y = y
+
+    print(left_y, forward_y, right_y)
+
+    if right_points > left_points and right_points > forward_points and right_y < y - 1:
+        return actions.RIGHT
+    elif left_points > right_points and left_points > forward_points and left_y < y - 1:
+        return actions.LEFT
+    elif forward_points > left_points and forward_points > right_points and forward_y < y - 1:
+        return actions.NONE
+    elif right_y < y - 1:
+        return actions.RIGHT
+    elif left_y < y - 1:
+        return actions.LEFT
+    else:
+        return actions.NONE
